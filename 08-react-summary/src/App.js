db0b9f688f1ddb42c83604730ecb932087768b76
amitbhalla <amit9815@gmail.com>
2021-09-28 10:42:20 +0530

    Router added

--- 08-react-summary/src/App.js
+++ 08-react-summary/src/App.js
@@ -1,5 +1,26 @@
-function App() {
-  return <div></div>;
+import { Fragment } from "react";
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetups from "./pages/AllMeetups";
+import NewMeetup from "./pages/NewMeetup";
+import Favorites from "./pages/Favorites";
+
+const App = () => {
+  return (
+    <Fragment>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetups />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetup />
+        </Route>
+        <Route path="/favorites">
+          <Favorites />
+        </Route>
+      </Switch>
+    </Fragment>
+  );
 }
 
 export default App;
