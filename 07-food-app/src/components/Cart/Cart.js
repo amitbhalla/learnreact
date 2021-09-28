1a2bb5954c82dc663f70e997a4ff674718bebe20
amitbhalla <amit9815@gmail.com>
2021-09-28 22:26:09 +0530

    CartProvider added

--- 07-food-app/src/components/Cart/Cart.js
+++ 07-food-app/src/components/Cart/Cart.js
@@ -1,5 +1,4 @@
 import Modal from "../UI/Modal";
-
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
@@ -22,8 +21,8 @@ const Cart = (props) => {
                 <span>35.62</span>
             </div>
             <div className={classes.actions}>
-                <button lassName={classes["button--alt"]} onClick={props.onClose}>Close</button>
-                <button lassName={classes.button}>Order</button>
+                <button className={classes["button--alt"]} onClick={props.onClose}>Close</button>
+                <button className={classes.button}>Order</button>
             </div>
         </Modal>
     );
